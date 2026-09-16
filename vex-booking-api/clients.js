c4d5188e4cc318e9f9e0f795d93da94241327c7b
zervexx <akillabt1@gmail.com>
2026-09-16 16:41:45 +0300

    feat: add NOIR restaurant client

--- vex-booking-api/clients.js
+++ vex-booking-api/clients.js
@@ -12,6 +12,7 @@ export const CLIENTS = {
     telegramChatIdKey: "TELEGRAM_CHAT_ID",
     timezone: "Europe/Riga",
     booking: {
+      type: "service",
       minNoticeMinutes: 0,
       maxDaysAhead: 30,
       slotIntervalMinutes: 30,
@@ -28,6 +29,33 @@ export const CLIENTS = {
       "10:00", "10:30", "11:00", "11:30", "12:00", "12:30",
       "13:00", "14:00", "14:30", "15:00", "16:00", "17:00",
       "18:00", "19:00", "20:00", "21:00"
+    ]
+  },
+
+  noir: {
+    id: "noir",
+    enabled: true,
+    name: "NOIR",
+    siteUrl: "https://zervexx.github.io/zervex",
+    telegramTokenKey: "NOIR_TELEGRAM_BOT_TOKEN",
+    telegramChatIdKey: "NOIR_TELEGRAM_CHAT_ID",
+    timezone: "Europe/Moscow",
+    booking: {
+      type: "restaurant",
+      minNoticeMinutes: 0,
+      maxDaysAhead: 30,
+      slotIntervalMinutes: 30,
+      workingDays: [1, 2, 3, 4, 5, 6, 7],
+      workingHours: { start: "12:00", end: "00:00" },
+      address: "Тверская, 12, Москва",
+      maxGuests: 12
+    },
+    services: [],
+    slotTimes: [
+      "12:00", "12:30", "13:00", "13:30", "14:00", "14:30",
+      "15:00", "15:30", "16:00", "16:30", "17:00", "17:30",
+      "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
+      "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"
     ]
   }
 };
